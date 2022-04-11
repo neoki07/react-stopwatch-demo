ce2c2765387f8f73a972f0f32f665e127d05e682
ot07 <ot07neoki@gmail.com>
2022-04-12 01:09:08 +0900

    ✨ ストップウォッチを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,85 @@
+import { ActionIcon } from "@mantine/core";
+import { useInterval } from "use-interval";
+import { useState } from "react";
+
 const App = () => {
-  return <div>hello, world!</div>;
+  const [playing, setPlaying] = useState(false);
+  const [time, setTime] = useState(0);
+
+  useInterval(() => {
+    if (playing) {
+      setTime((prev) => prev + 0.01);
+    }
+  }, 10);
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ display: "flex" }}>
+        <div style={{ margin: "0 5px", fontSize: "50px" }}>
+          {Math.floor(time.toFixed(2) / 60)
+            .toString()
+            .padStart(2, "0")}
+        </div>
+        <div style={{ margin: "0 5px", fontSize: "50px" }}>:</div>
+        <div style={{ margin: "0 5px", fontSize: "50px" }}>
+          {Math.floor(time.toFixed(2) % 60)
+            .toString()
+            .padStart(2, "0")}
+        </div>
+        <div style={{ margin: "0 5px", fontSize: "50px" }}>.</div>
+        <div style={{ margin: "0 5px", fontSize: "50px" }}>
+          {time.toFixed(2).split(".")[1]}
+        </div>
+      </div>
+      <div style={{ display: "flex", alignItems: "flex-end" }}>
+        <ActionIcon
+          variant="outline"
+          size={60}
+          radius={"50%"}
+          style={{
+            margin: "0 10px",
+          }}
+        >
+          DUMMY
+        </ActionIcon>
+        <ActionIcon
+          variant="outline"
+          size={80}
+          radius={"50%"}
+          style={{
+            margin: "0 10px",
+          }}
+          onClick={() => setPlaying(!playing)}
+        >
+          {playing ? "STOP" : "START"}
+        </ActionIcon>
+        <ActionIcon
+          variant="outline"
+          size={60}
+          radius={"50%"}
+          style={{
+            margin: "0 10px",
+          }}
+          onClick={() => {
+            if (time > 0) {
+              setPlaying(false);
+              setTime(0);
+            }
+          }}
+        >
+          RESET
+        </ActionIcon>
+      </div>
+    </div>
+  );
 };
 
 export default App;
