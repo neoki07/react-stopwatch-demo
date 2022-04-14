39491ca8ca36ded5de1096508028457f168d610e
ot07 <ot07neoki@gmail.com>
2022-04-15 00:09:24 +0900

    💄 ボタンの色を変更

--- src/App.jsx
+++ src/App.jsx
@@ -46,17 +46,21 @@ const App = () => {
         <div className="mx-3">{getCentiSecondsStr(time)}</div>
       </div>
       <div className="my-6 flex items-center">
-        <button className="w-24 h-24 mx-8 text-base font-bold text-gray-500 border-2 border-gray-500 rounded-full hover:bg-gray-50 active:bg-gray-100 active:translate-y-0.5">
+        <button className="w-24 h-24 mx-8 text-base font-bold text-gray-600 border-2 border-gray-400 bg-gray-100 rounded-full hover:bg-gray-200 active:translate-y-0.5">
           DUMMY
         </button>
         <button
-          className="w-32 h-32 mx-8 text-2xl font-bold text-gray-500 border-2 border-gray-500 rounded-full hover:bg-gray-50 active:bg-gray-100 active:translate-y-0.5"
+          className={`w-32 h-32 mx-8 text-2xl font-semibold text-white border-2 rounded-full ${
+            running
+              ? "border-rose-700 bg-rose-500 hover:bg-rose-600"
+              : "border-sky-700 bg-sky-500 hover:bg-sky-600"
+          } active:translate-y-0.5`}
           onClick={handleOnStart}
         >
           {running ? "STOP" : "START"}
         </button>
         <button
-          className="w-24 h-24 mx-8 text-base font-bold text-gray-500 border-2 border-gray-500 rounded-full hover:bg-gray-50 active:bg-gray-100 active:translate-y-0.5"
+          className="w-24 h-24 mx-8 text-base font-bold text-gray-600 border-2 border-gray-400 bg-gray-100 rounded-full hover:bg-gray-200 active:translate-y-0.5"
           onClick={handleOnReset}
         >
           RESET
