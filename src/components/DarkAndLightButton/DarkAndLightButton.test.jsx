a7129f8fa21fbc1dc39a8aa2a05d61c185064c4d
ot07 <ot07neoki@gmail.com>
2022-04-19 03:00:59 +0900

    ✅ ボタンコンポーネントのテストをApp.test.jsxから移動

---
+++ src/components/DarkAndLightButton/DarkAndLightButton.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import DarkAndLightButton from "src/components/DarkAndLightButton";
+import { useState } from "react";
+
+const MockDarkAndLightButton = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  return (
+    <DarkAndLightButton isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+  );
+};
+
+describe("Dark/Light Button", () => {
+  it("should render light button when dark button is clicked once", () => {
+    render(<MockDarkAndLightButton />);
+    const darkOrLightButton = screen.getByRole("button", { name: "DARK" });
+    fireEvent.click(darkOrLightButton);
+    expect(darkOrLightButton).toHaveTextContent("LIGHT");
+  });
+
+  it("should render dark button when dark button is clicked twice", () => {
+    render(<MockDarkAndLightButton />);
+    const darkOrLightButton = screen.getByRole("button", { name: "DARK" });
+    fireEvent.click(darkOrLightButton);
+    fireEvent.click(darkOrLightButton);
+    expect(darkOrLightButton).toHaveTextContent("DARK");
+  });
+});
---
+++ src/components/StartAndStopButton/StartAndStopButton.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import StartAndStopButton from "src/components/StartAndStopButton";
+import { useState } from "react";
+
+const MockStartAndStopButton = () => {
+  const [running, setRunning] = useState(false);
+  return <StartAndStopButton running={running} setRunning={setRunning} />;
+};
+
+describe("Start/Stop Button", () => {
+  it("should render stop button when start button is clicked once", () => {
+    render(<MockStartAndStopButton />);
+    const startOrStopButton = screen.getByRole("button", { name: "START" });
+    fireEvent.click(startOrStopButton);
+    expect(startOrStopButton).toHaveTextContent("STOP");
+  });
+
+  it("should render start button when start button is clicked twice", () => {
+    render(<MockStartAndStopButton />);
+    const startOrStopButton = screen.getByRole("button", { name: "START" });
+    fireEvent.click(startOrStopButton);
+    fireEvent.click(startOrStopButton);
+    expect(startOrStopButton).toHaveTextContent("START");
+  });
+});
